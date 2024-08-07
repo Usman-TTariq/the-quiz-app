ced1f4e7e4446f8b99dd16abcf01f499a103e6b4
USMAN TARIQ <utariq769@gmail.com>
2024-08-07 18:27:57 +0500

    third commit

--- src/app/components/quiz/page.tsx
+++ src/app/components/quiz/page.tsx
@@ -2,20 +2,45 @@
 
 import { useState } from "react";
 import questions from "../../questions.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
   const [isOptionSelected, setIsOptionSelected] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(undefined);
   const [answerRemarks, setAnswerRemarks] = useState("");
   const totalQuestions = questions.length;
   const nextQuestion =
     currentQuestion < totalQuestions ? currentQuestion + 1 : currentQuestion;
 
   const questionText = decodeURIComponent(questions[currentQuestion].question);
+  const questionCategory = decodeURIComponent(
+    questions[currentQuestion].category
+  );
+  const questionDifficulty = decodeURIComponent(
+    questions[currentQuestion].difficulty
+  );
 
   function handleNextQuestion() {
     setCurrentQuestion(currentQuestion + 1);
   }
+
+  // const shuffleArray = (array: string[]) => {
+  //   for (let i = array.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [array[i], array[j]] = [array[j], array[i]];
+  //   }
+  //   return array;
+  // };
+
+  // const shuffleArray = (array) => {
+  //   return array
+  //     .map((item) => ({ sort: Math.random(), value: item }))
+  //     .sort((a, b) => a.sort - b.sort)
+  //     .map((item) => item.value);
+  // };
 
   const temp = questions.map((item) => {
     const decodedCorrectAnswer = decodeURIComponent(item.correct_answer);
@@ -30,14 +55,20 @@ export default function Quiz() {
     };
   });
 
-  const questionCorrectAnswer = decodeURIComponent(questions[currentQuestion].correct_answer);
+  // const shuffledOptions = shuffleArray(temp[currentQuestion].options);
 
-const checkingCorrectAnswer = (selectedOption: string) => {
+  const questionCorrectAnswer = decodeURIComponent(
+    questions[currentQuestion].correct_answer
+  );
+
+  const checkingCorrectAnswer = (selectedOption: string, index: number) => {
+    setCurrentIndex(index);
     const checkingAnswer = selectedOption === questionCorrectAnswer;
     if (checkingAnswer) {
-      setAnswerRemarks("correct answer");
+      setAnswerRemarks("correct!");
+      setScore(score + 1);
     } else {
-      setAnswerRemarks("wrong answer");
+      setAnswerRemarks("Sorry!");
     }
     setIsOptionSelected(true);
   };
@@ -45,69 +76,164 @@ const checkingCorrectAnswer = (selectedOption: string) => {
   const progress = ((currentQuestion + 1) / questions.length) * 100;
   const adjustedProgress = Math.max(progress, 5);
 
+  const percentageScore = (score / totalQuestions) * 100;
+
   return (
     <div>
-      <section className="pt-4">
-        <div className="container mx-auto px-5">
-          <div className="flex flex-wrap">
-            <div className="w-full">
-              <h1 className="text-2xl uppercase font-semibold text-blue-700 text-center">
-                making quiz game
-              </h1>
-              <div className="h-8 bg-slate-100 mt-3">
-                <div
-                  className="w-1/4 h-full bg-green-500"
-                  style={{ width: `${adjustedProgress}%` }}
-                ></div>
-              </div>
-              <div className="flex justify-between items-center">
-                <div className="w-1/6 bg-blue-700 p-3 rounded-lg my-4">
-                  <h4 className="text-white text-center text-sm">
-                    Question <span>{nextQuestion}</span> of{" "}
-                    <span>{totalQuestions}</span>
-                  </h4>
+      {currentQuestion <= totalQuestions - 1 ? (
+        <section className="pt-4">
+          <div className="container mx-auto w-1/2 border-4 border-slate-200">
+            <div className="flex flex-wrap">
+              <div className="w-full">
+                <div className="h-6 bg-slate-100">
+                  <div
+                    className="w-1/4 h-full bg-slate-400"
+                    style={{ width: `${adjustedProgress}%` }}
+                  ></div>
                 </div>
-                <div>
-                  <button
-                    // onClick={handleNextQuestion}
-                    onClick={() => {
-                        if (isOptionSelected) {
-                          setCurrentQuestion((prev) => (prev + 1) % temp.length);
-                          setIsOptionSelected(false);
-                          setAnswerRemarks("");
-                        } else {
-                          alert("Please select an option before moving to the next question.");
-                        }
-                      }}
-                      disabled={!isOptionSelected}
-                    className="py-3 px-5 text-center bg-blue-700 text-white text-xs uppercase"
-                  >
-                    next question
-                  </button>
+
+                <div className="px-14 pb-10">
+                  <div className="flex justify-between items-center">
+                    <div className="pt-3 mt-4">
+                      <h4 className="text-xl font-semibold">
+                        Question <span>{nextQuestion}</span> of{" "}
+                        <span>{totalQuestions}</span>
+                      </h4>
+                    </div>
+                  </div>
+                  <div>
+                    <h6 className="text-xs text-slate-400">
+                      {questionCategory}
+                    </h6>
+                  </div>
+                  <div>
+                    {questionDifficulty == "easy" ? (
+                      <h6>
+                        <span className="text-xs">
+                          <FontAwesomeIcon icon={faStar} />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                        </span>
+                      </h6>
+                    ) : questionDifficulty == "medium" ? (
+                      <h6>
+                        <span className="text-xs">
+                          <FontAwesomeIcon icon={faStar} />
+                          <FontAwesomeIcon icon={faStar} />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                        </span>
+                      </h6>
+                    ) : questionDifficulty == "hard" ? (
+                      <h6>
+                        <span className="text-xs">
+                          <FontAwesomeIcon icon={faStar} />
+                          <FontAwesomeIcon icon={faStar} />
+                          <FontAwesomeIcon icon={faStar} />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                          <FontAwesomeIcon
+                            className="text-slate-200"
+                            icon={faStar}
+                          />
+                        </span>
+                      </h6>
+                    ) : (
+                      <h6></h6>
+                    )}
+                  </div>
+                  <div className="py-3">
+                    <h2>{questionText}</h2>
+                  </div>
+                  <div className="flex flex-row flex-wrap justify-between gap-4">
+                    {temp[currentQuestion].options.map(
+                      (singleOption, index: number) => (
+                        <button
+                          key={index}
+                          className={`border-2 p-2 mb-2 border-slate-500 rounded-md text-sm w-2/5  ${
+                            index == currentIndex
+                              ? "bg-black border-black text-white"
+                              : ""
+                          }  focus:outline-none  transition-all ease-in-out duration-300`}
+                          onClick={() =>
+                            checkingCorrectAnswer(singleOption, index)
+                          }
+                          disabled={isOptionSelected}
+                        >
+                          {singleOption}
+                        </button>
+                      )
+                    )}
+                  </div>
+                  <div>
+                    <h1 className="text-lg py-3 uppercase font-bold text-center">
+                      <span>{answerRemarks}</span>
+                    </h1>
+
+                    {isOptionSelected && (
+                      <div className="text-center">
+                        <button
+                          onClick={() => {
+                            if (isOptionSelected) {
+                              setCurrentQuestion(
+                                (prev) => (prev + 1) % temp.length
+                              );
+                              setIsOptionSelected(false);
+                              setAnswerRemarks("");
+                              setCurrentIndex(undefined);
+                            } else {
+                              alert(
+                                "Please select an option before moving to the next question."
+                              );
+                            }
+                          }}
+                          disabled={!isOptionSelected}
+                          className="py-3 px-5 text-center bg-zinc-100 border-2 font-medium border-black rounded text-black text-xs capitalize"
+                        >
+                          next question
+                        </button>
+                      </div>
+                    )}
+
+                    <div>
+                      Score: {score} / {totalQuestions} (
+                      {percentageScore.toFixed(2)}%)
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="py-3">
-                <h2>{questionText}</h2>
-              </div>
-              <div className="flex flex-col w-1/4">
-                {temp[currentQuestion].options.map((singleOption, index) => (
-                  <button
-                    key={index}
-                    className="border-2 p-1 mb-2 border-blue-700 rounded-md text-sm hover:bg-blue-700 hover:text-white transition-all ease-in-out duration-300"
-                    onClick={() => checkingCorrectAnswer(singleOption)}
-                    disabled={isOptionSelected}
-                  >
-                    {singleOption}
-                  </button>
-                ))}
-              </div>
-              <div>
-                <h1 className="text-lg py-3 uppercase font-bold"><span>{answerRemarks}</span></h1>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <h6>quiz is completed.</h6>
+      )}
     </div>
   );
 }
